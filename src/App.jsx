8024c440cd5cfdc126dfc90add01786b2405ec59
ïmtheu <matheusfrmorais@hotmail.com>
2024-04-28 20:20:58 -0300

    feat: basic structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Main from "./components/main";
+import Nav from "./components/nav";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="/logo.jpg" alt="Little Lemon" />
+    </header>
+  );
+};
+
+export default Header;
